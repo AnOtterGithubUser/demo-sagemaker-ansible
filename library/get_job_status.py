6f2d4016bf03ff15c019310ff24f8ff0b92f335d
AnOtterGithubUser <thomaspesneau@gmail.com>
2020-12-06 18:20:39 +0100

    Format code with black

--- library/get_job_status.py
+++ library/get_job_status.py
@@ -12,34 +12,42 @@ def get_monitored_job(training_job_summaries, training_job_arn):
 
 
 def get_job_status(
-        aws_access_key,
-        aws_secret_access_key,
-        security_token,
-        training_job_arn,
-        wait_between_tries=1,
-        duration_threshold=360
+    aws_access_key,
+    aws_secret_access_key,
+    security_token,
+    training_job_arn,
+    wait_between_tries=1,
+    duration_threshold=360,
 ):
-    client = boto3.client("sagemaker",
-                          aws_access_key_id=aws_access_key,
-                          aws_secret_access_key=aws_secret_access_key,
-                          aws_session_token=security_token
-                          )
+    client = boto3.client(
+        "sagemaker",
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=security_token,
+    )
 
     completed = False
 
     start_time = time.time()
     while not completed:
         time.sleep(wait_between_tries)
-        training_jobs = client.list_training_jobs(
-            SortBy='CreationTime'
-        )
+        training_jobs = client.list_training_jobs(SortBy="CreationTime")
         try:
-            job_to_monitor = get_monitored_job(training_jobs["TrainingJobSummaries"], training_job_arn)
+            job_to_monitor = get_monitored_job(
+                training_jobs["TrainingJobSummaries"], training_job_arn
+            )
         except ValueError:
-            return {"TrainingJobArn": training_job_arn, "Status": "Error: Found no job with arn %s" % training_job_arn}
+            return {
+                "TrainingJobArn": training_job_arn,
+                "Status": "Error: Found no job with arn %s" % training_job_arn,
+            }
         completed = job_to_monitor["TrainingJobStatus"] == "Completed"
         if time.time() - start_time > duration_threshold:
-            return {"TrainingJobArn": training_job_arn, "Status": "Error: Exceeded maximum duration (%s s)" % duration_threshold}
+            return {
+                "TrainingJobArn": training_job_arn,
+                "Status": "Error: Exceeded maximum duration (%s s)"
+                % duration_threshold,
+            }
 
     return {"TrainingJobArn": training_job_arn, "Status": "Completed"}
 
@@ -51,7 +59,7 @@ def run_get_job_status():
         security_token=dict(type="str"),
         training_job_arn=dict(type="str"),
         wait_between_tries=dict(type="int"),
-        duration_threshold=dict(type="int")
+        duration_threshold=dict(type="int"),
     )
 
     module = AnsibleModule(
@@ -64,7 +72,7 @@ def run_get_job_status():
         module.params["security_token"],
         module.params["training_job_arn"],
         module.params["wait_between_tries"],
-        module.params["duration_threshold"]
+        module.params["duration_threshold"],
     )
 
     module.exit_json(**response)
